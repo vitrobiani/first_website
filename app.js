ec4abdc5a1c69170d2293cfccc77e95b2eca3b79
vitrobiani <vitrobiani@gmail.com>
2024-08-29 00:21:55 +0300

    making footer notes dynamic, signing up to formspree to deal with messages coming from the leave a message option

--- app.js
+++ app.js
@@ -5,3 +5,15 @@ menu.addEventListener('click', function() {
   menu.classList.toggle('is-active');
   menuLinks.classList.toggle('active');
 });
+
+fetch("footer.html")
+.then(response => response.text())
+.then(data => {
+    document.getElementById("footer").innerHTML = data;
+});
+
+fetch("nav.html")
+.then(response => response.text())
+.then(data => {
+    document.getElementById("nav").innerHTML = data;
+});
