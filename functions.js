36a8a469b1130c511be998d618aae11dd8924ff0
vitrobiani <vitrobiani@gmail.com>
2024-08-30 02:33:15 +0300

    now that we fixed that i return the things back

--- functions.js
+++ functions.js
@@ -12,11 +12,11 @@ let savedValue = 'empty.html';
 function saveValue(value) {
     console.log(value + " saved");
     savedValue = value;
-    // localStorage.setItem('savedValue', value);
+    localStorage.setItem('savedValue', value);
 }
 
 function loadhtmlSavedValue(id) {
-    // let savedValue1 = localStorage.getItem('savedValue') || 'empty.html';
+    let savedValue1 = localStorage.getItem('savedValue') || 'empty.html';
     console.log(savedValue1);
     console.log("omer");
 
