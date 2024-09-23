08d4a4352366b3798eb39025b0035f5a48d6a38c
vitrobiani <vitrobiani@gmail.com>
2024-09-24 00:45:34 +0300

    working on the docs

---
+++ nixwsldoc/functions.js
@@ -0,0 +1,34 @@
+
+// Toggle sidebar open/close
+function toggleSidebar() {
+    const sidebar = document.getElementById('sidebar');
+    const mainContent = document.getElementById('main-content');
+    sidebar.classList.toggle('active');
+    mainContent.classList.toggle('shifted');
+}
+
+// Toggle the visibility of submenu items
+function toggleSubMenu(subMenuId) {
+    const subMenu = document.getElementById(subMenuId);
+    subMenu.style.display = subMenu.style.display === 'block' ? 'none' : 'block';
+}
+
+let nixDocSavedValue = 'empty.html';
+
+function saveValue(value) {
+    nixDocSavedValue = value;
+    localStorage.setItem('nixDocSavedValue', value);
+    loadhtmlSavedValue("main-content");
+}
+
+function loadhtmlSavedValue(id) {
+    let savedValue1 = localStorage.getItem('nixDocSavedValue') || 'empty.html';
+    console.log(savedValue1);
+
+    fetch(savedValue1)
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById(id).innerHTML = data;
+    })
+    .catch(error => console.error('Error fetching the footer:', error));
+}
